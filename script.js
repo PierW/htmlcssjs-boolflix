cfbe08c39dad8e508b7c27ce2ac8188806f1976e
Pier.W <factory666@live.it>
2019-03-26 16:57:55 +0100

    Milestone stars ok

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ function getTitle() {
   var input = $("input");
   var value = input.val();
   getFilm(value);
+  input.val("");
+}
+
+function pressEnter(e) {
+
+  var me = $(this);
+  if (e.which == 13) {
+
+    getTitle();
+  }
 }
 
 function getFilm(string) {
@@ -39,6 +49,9 @@ function outputData(object) {
   var container = $("ul");
       nresults.text("Risultati trovati: " + numberResults);
 
+  var li = $(".container li");
+      li.remove();
+
   for (var i = 0; i < results.length; i++) {
 
     var result = results[i];
@@ -48,9 +61,36 @@ function outputData(object) {
       lingua : result.original_language,
       voto : result.vote_average
     }
+
     var fullHtml = template(data);
     container.append(fullHtml);
+    fillStars(data.voto);
+    console.log(transformNumber(data.voto))
   }
+}
+
+function transformNumber(number) {
+
+    return Math.ceil(number/2);//I NUMERI VANNO DA 0 A 5
+}
+
+function fillStars(number) {
+
+  var voteIndexDOM = transformNumber(number);
+
+  var empty = "far";
+  var full = "fas";
+  var stars = $(".container li .check");
+
+  if (voteIndexDOM !== 0) { //SE è 0 LO BUTTO (COSì HO VALORI DA 1 A 5)
+    voteIndexDOM -= 1; // COSì HO VALORI DA 0 A 4 CHE CORRISPONDONO AGLI INDICI DELLE 5 STELLE
+    for (var i = 0; i <= voteIndexDOM; i++) {
+
+      stars.eq(i).removeClass(empty);
+      stars.eq(i).addClass(full);
+    }
+  }
+  stars.removeClass("check") //CLASSE DI CONTROLLO - MI GARANTISCO CHE AD OGNI GIRO HO SOLO IL TEMPLATE CHE STO PER SCRIVERE
 }
 
 function init() {
@@ -58,4 +98,6 @@ function init() {
  var button = $("button");
  button.click(getTitle);
 
+ var input = $("input");
+ input.keyup(pressEnter);
 }
